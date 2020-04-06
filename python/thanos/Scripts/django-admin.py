206c54bcc0fcadcecd7cf7290c2acef26469b72b
lthorpedo <lthorpedo@users.noreply.github.com>
2020-04-06 11:49:05 -0600

    installing the python app and django framework stuff

---
+++ python/thanos/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\1socc\source\repos\gofundmecode\python\thanos\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
