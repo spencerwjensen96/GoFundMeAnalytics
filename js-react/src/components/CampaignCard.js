d2f2ec038f09ff52919e38ab67faa9145fabc515
Spencer W. Jensen <spencer.jensen@Spencers-MBP-5.app.byu.edu>
2020-04-07 10:33:37 -0600

    card and details outline

---
+++ js-react/src/components/CampaignCard.js
@@ -0,0 +1,19 @@
+import React, {useState, useContext} from 'react'
+import {useRouteMatch} from "react-router-dom";
+import {Row, Col, Image, Button} from 'react-bootstrap'
+
+import AppContext from '../context'
+
+function CampaignCard(props){
+
+    const context = useContext(AppContext)
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default CampaignCard
---
+++ js-react/src/components/CampaignDetails.js
@@ -0,0 +1,21 @@
+import React, {useState, useContext} from 'react'
+import {useRouteMatch} from "react-router-dom";
+import {Row, Col, Image, Button} from 'react-bootstrap'
+//import PRODUCTS from '../products'
+import AppContext from '../context'
+
+function CampaignDetails(props){
+
+    const context = useContext(AppContext)
+
+    //filter the Array and get the product that we are displaying details for
+    
+
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default CampaignDetails
