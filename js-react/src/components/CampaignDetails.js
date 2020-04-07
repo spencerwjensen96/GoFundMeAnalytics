e57242fe6ff8ded8e1832294a30239e584abcb2e
Spencer W. Jensen <spencer.jensen@Spencers-MBP-5.app.byu.edu>
2020-04-07 10:48:38 -0600

    few components

--- js-react/src/components/CampaignDetails.js
+++ js-react/src/components/CampaignDetails.js
@@ -1,18 +1,36 @@
 import React, {useState, useContext} from 'react'
 import {useRouteMatch} from "react-router-dom";
 import {Row, Col, Image, Button} from 'react-bootstrap'
-//import PRODUCTS from '../products'
+
 import AppContext from '../context'
+import NotFound from './NotFound'
 
 function CampaignDetails(props){
 
     const context = useContext(AppContext)
 
     //filter the Array and get the product that we are displaying details for
-    
+    let CampArray = Object.values(props.campaign)
+
+    const match = useRouteMatch()
+
+    if(match){
+        CampArray = CampArray.filter(c => {
+            return String(c.id) === match.params.id
+        })
+    }
+
+    let camp = CampArray[0]
+
+    if(camp === undefined){
+        return(
+            <NotFound/>
+        )
+    }
 
     return(
-        <div>
+        <div className="flex">
+            <h1>{camp.title}</h1>
 
         </div>
     )
