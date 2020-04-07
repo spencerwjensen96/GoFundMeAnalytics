dfa20f7ebe7cbc246bde5b49cee9056172901105
Spencer W. Jensen <spencer.jensen@Spencers-MBP-5.app.byu.edu>
2020-04-07 10:07:42 -0600

    slider style

--- js-react/src/components/Header.js
+++ js-react/src/components/Header.js
@@ -7,12 +7,12 @@ export default function Header(props) {
 return(
     <bs.Navbar expand="lg" style={{backgroundColor: 'white'}}>
       <bs.Col md={1}></bs.Col>
-        <bs.Col md={2}>
+        <bs.Col md={6}>
           <bs.Navbar.Brand href="/" style={{color: '#5cb85c'}}>Go Fund Me Analytics</bs.Navbar.Brand>
         </bs.Col>
-        <bs.Col md={8}>
+        <bs.Col md={4}>
           <bs.Form inline>
-            <bs.FormControl style={{width: '80%'}} type="text" placeholder="Search" className="m-1" />
+            <bs.FormControl style={{float: 'right', width: '70%'}} type="text" placeholder="Search" className="m-1" />
             <bs.Button variant="outline-success">Search</bs.Button>
           </bs.Form>
         </bs.Col>
