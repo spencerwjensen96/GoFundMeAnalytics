21dc847e5a1b0f26502e55fd9db1eec957495d23
lthorpedo <lthorpedo@users.noreply.github.com>
2020-04-08 15:49:28 -0600

    stuff

--- js-react/src/components/Footer.js
+++ js-react/src/components/Footer.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default function Footer(props) {
     return (
-      <div style={{backgroundColor: 'white', borderTop: 'solid 2px lightgray', textAlign: 'center'}}>
+      <div style={{backgroundColor: 'white', textAlign: 'center'}} className="m-3">
         <p className="text-black">&copy; JESL Inc., 2020</p>
       </div>
     );
--- python/gofundmeapi/api/views.py
+++ python/gofundmeapi/api/views.py
@@ -18,9 +18,10 @@ class Campaigns(APIView):
     @csrf_exempt
     def get(self, request, format=None):
         campaigns = Campaign.objects.all()
+        print(request.body)
         if len(campaigns) > 0:
-            if len(campaigns) > 100:
-                campaigns = Campaign.objects.filter()[:100]
+            # if len(campaigns) > 100:
+            #     campaigns = Campaign.objects.filter()[:100]
             serializer = CampaignSerializer(campaigns, many=True)
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
