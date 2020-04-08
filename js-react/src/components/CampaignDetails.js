1bccad658ca190fa31635d09256d9923c377b7cf
Emma <emmahlowe@gmail.com>
2020-04-08 17:56:40 -0600

    did NOT get the tool tip working...

--- js-react/src/components/CampaignDetails.js
+++ js-react/src/components/CampaignDetails.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 import AppContext from '../context'
 import NotFound from './NotFound'
 
-const card = {background: '#ECECEC', borderRadius: '8px', marginTop: '2rem', boxShadow: '-2px 2px 5px #9E9E9E'}
+const card = {background: '#F0F0F0', borderRadius: '8px', marginTop: '2rem', boxShadow: '-2px 2px 5px #9E9E9E'}
 
 function CampaignDetails(props){
 
@@ -39,40 +39,43 @@ function CampaignDetails(props){
     month[10] = "November";
     month[11] = "December";
     console.log(date)
+    const title = {'font-family': 'Roboto', color: '#141414'}
     
     return(
         <Container>
-            <Row xs={12} style={{height: '500px', textAlign: 'center', background: 'black', borderRadius: '5px'}} className="d-flex justify-content-center">
+            <h2 className="text-center" style={title}><strong>{camp.title}</strong></h2>
+            <Row xs={12} style={{height: '500px', textAlign: 'center', background: 'black', borderRadius: '5px'}} className="d-flex justify-content-center mb-3">
                 <Image height="500px" src={camp.imageUrl} ></Image>
             </Row>
 
             <Row>
                 <Col xs={6}>
-                    <Row className="p-1 mb-1">
-                        <i className="fas fa-user-circle"></i>
-                        <h5 className="mt-2 ml-3">{camp.userFirst} {camp.userLast} organized this fundraiser</h5>
+                    <Row className="p-1 mb-3">
+                        <i className="fas fa-user-circle fa-lg"></i>
+                        <h5 style={{lineHeight: '10%'}} className="mt-2 ml-4">{camp.userFirst} {camp.userLast} organized this fundraiser</h5>
                         </Row>
-                    <Row className="p-1 mb-1">
-                        <i class="fas fa-heart"></i>
-                        <h5 className="mt-2 ml-3">{camp.campaignHearts} people liked this fundraiser</h5>
+                    <Row className="p-1 mb-3">
+                        <i style={{color: 'pink'}} class="fas fa-heart fa-lg"></i>
+                        <h5 style={{lineHeight: '10%'}} className="mt-2 ml-4">{camp.campaignHearts} people liked this fundraiser</h5>
                         </Row>
-                    <Row className="p-1 mb-1">
-                        <i class="fas fa-photo-video"></i>
-                        <h5 className="mt-2 ml-3">{camp.socialShareTotal} shares on social media</h5>
+                    <Row className="p-1 mb-2">
+                        <i style={{color: 'gold'}} class="fas fa-photo-video fa-lg"></i>
+                        <h5 style={{lineHeight: '10%'}} className="mt-2 ml-3">{camp.socialShareTotal} shares on social media</h5>
                         </Row>
-                    <Row className="p-1 mb-1">
-                        <i class="fas fa-dollar-sign"></i>
-                        <h5 className="mt-2 ml-3">{camp.donators} donators with ~${parseInt(camp.moneyPerDonor).toFixed(2)} per donator</h5>
+                    <Row className="p-1 mb-2">
+                        <i style={{color: 'green'}} class="fas fa-dollar-sign fa-lg"></i>
+                        <h5 style={{lineHeight: '10%'}} className="mt-2 ml-4">{camp.donators} donators with ~${parseFloat(camp.moneyPerDonor).toFixed(2)} per donator</h5>
                     </Row>
                 </Col>
                 <Col xs={6}>
-                    <Container style={card}>
-                        <h1>{camp.title}</h1>
-                        <ProgressBar style={{height: '10px'}} variant="success" now={camp.percentComplete} key={1}/>
+                    <div style={card} className="p-2">
                         <div className="inline p-2">
-                            <h4><b>${camp.currentAmount}</b> raised of ${camp.goal} goal</h4>
+                            <h4 style={{display: 'inline'}}><b>${camp.currentAmount}</b></h4><p style={{display: 'inline'}}> raised of ${camp.goal} goal</p>
                         </div>
-                    </Container>
+                        <ProgressBar style={{height: '10px'}} variant="success" now={camp.percentComplete} key={1}/>
+                        <p className="showing"><i className="fas fa-info-circle"></i> Quality Score: {camp.quality}</p>
+                    </div>
+                    <p className="notshowing">Quality Score Determination:</p>
                 </Col>
             </Row>
             <Row>
@@ -82,6 +85,7 @@ function CampaignDetails(props){
                 <h5>Created on {month[date.getMonth()]} {date.getUTCDay()}, {date.getUTCFullYear()} | status: {camp.status ? "active" : "not active"}</h5>
             </Row>
         </Container>
+        
     )
 }
 
